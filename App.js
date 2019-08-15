c5ad04941c63a07dd02084a14db0a35f9d1519f8
lancerutkin <lance.rutkin@gmail.com>
2019-08-15 17:05:40 -0400

    Style map and fetch stores

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,28 +14,30 @@ import {
   View,
   StatusBar,
   Button,
-  Animated
-} from 'react-native';
+  Animated,
+  Text,
+  Dimensions
+} from "react-native";
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-import MapView from 'react-native-maps';
+import MapView, { Marker, Callout } from "react-native-maps";
 
-import Geolocation from '@react-native-community/geolocation';
+import Geolocation from "@react-native-community/geolocation";
+
+const { height, width } = Dimensions.get("screen");
 
 const App = () => {
-
   const animatedTiming = 200;
-  const addressUrl = 'https://mvp.bartlebyapp.com/address';
+  const addressUrl = "https://blooming-springs-94129.herokuapp.com/address";
 
-  const [longitude, setLongitude] = useState(0.0000);
-  const [latitude, setLatitude] = useState(0.0000);
+  const [longitude, setLongitude] = useState(0.0);
+  const [latitude, setLatitude] = useState(0.0);
   const [latRange, setLatRange] = useState(0.015);
   const [lngRange, setLngRange] = useState(0.0121);
   const [canUpdate, setCanUpdate] = useState(false);
   const [fadeAnim, setFadeAnim] = useState(new Animated.Value(0));
+  const [stores, setStores] = useState([]);
   const [mapLoaded, setMapLoaded] = useState(false);
   const [isInitialLoad, setIsInitialLoad] = useState(true);
 
@@ -46,22 +48,38 @@ const App = () => {
     setLngRange(event.longitudeDelta);
   };
 
+  const fetchStores = (lat = latitude, lng = longitude) => {
+    fetch(
+      `${addressUrl}/?lat=${lat}&lng=${lng}&latRange=${latRange}&lngRange=${lngRange}`
+    )
+      .then(response =>
+        response.json().then(result => {
+          setStores(result);
+        })
+      )
+      .catch(err => console.error(err));
+  };
+
   Geolocation.setRNConfiguration({
-    authorizationLevel: 'whenInUse'
+    authorizationLevel: "whenInUse"
   });
 
   Geolocation.requestAuthorization();
 
-  Geolocation.getCurrentPosition(
-    ({ coords }) => {
-      setLongitude(coords.longitude);
-      setLatitude(coords.latitude);
-      setMapLoaded(true);
-    }, error => alert(error.message),
-    { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
-  );
+  if (isInitialLoad) {
+    Geolocation.getCurrentPosition(
+      ({ coords }) => {
+        setLongitude(coords.longitude);
+        setLatitude(coords.latitude);
+        setMapLoaded(true);
+        fetchStores(coords.latitude, coords.longitude);
+      },
+      error => alert(error.message),
+      { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
+    );
+  }
 
-  const allowUpdate = (event)=> {
+  const allowUpdate = event => {
     if (isInitialLoad) {
       setIsInitialLoad(false);
     } else {
@@ -70,21 +88,19 @@ const App = () => {
         toValue: 1,
         duration: animatedTiming
       }).start();
-      setCanUpdate(true)
+      setCanUpdate(true);
       console.log(latitude, longitude, latRange, lngRange);
     }
   };
 
   const update = () => {
     if (canUpdate) {
-      fetch(addressUrl, {
-        query: {
-          lat: latitude,
-          lng: longitude,
-          lngRange: lngRange,
-          latRange: latRange
-        }}).then(response => console.log(response))
-        .catch(err => console.error(err));
+      fetchStores();
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: animatedTiming
+      }).start();
+      setCanUpdate(false);
     }
   };
 
@@ -94,23 +110,71 @@ const App = () => {
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+          style={styles.scrollView}
+        >
           <View style={styles.body}>
             <View style={styles.container}>
-              {mapLoaded ? <MapView
-                showUserLocation={true}
-                followUserLocation={true}
-                style={styles.map}
-                initialRegion={{
-                  latitude: latitude,
-                  longitude: longitude,
-                  latitudeDelta: latRange,
-                  longitudeDelta: lngRange,
-                }}
-                onRegionChangeComplete={allowUpdate}
-              /> : null}
+              {mapLoaded ? (
+                <MapView
+                  showUserLocation={true}
+                  style={styles.map}
+                  initialRegion={{
+                    latitude: latitude,
+                    longitude: longitude,
+                    latitudeDelta: latRange,
+                    longitudeDelta: lngRange
+                  }}
+                  onRegionChangeComplete={allowUpdate}
+                >
+                  {stores.map(store => {
+                    return (
+                      <Marker
+                        key={store._id.toString()}
+                        coordinate={{
+                          latitude: store.lat,
+                          longitude: store.lng
+                        }}
+                      >
+                        <Callout>
+                          <View>
+                            <Text>{store.storeName}</Text>
+                            <Text>{`${store.address1}${
+                              store.address2 ? `, ${store.address2}` : ""
+                            }`}</Text>
+                            {store.minimumPurchase ? (
+                              <Text>
+                                Minimum purchase: ${store.minimumPurchase}
+                              </Text>
+                            ) : null}
+                            {store.chargeFlat ? (
+                              <Text>Flat fee: ${store.chargeFlat}</Text>
+                            ) : null}
+                            {store.chargePercent ? (
+                              <Text>
+                                Percentage fee: {store.chargePercent}%
+                              </Text>
+                            ) : null}
+                          </View>
+                        </Callout>
+                      </Marker>
+                    );
+                  })}
+                </MapView>
+              ) : null}
             </View>
-            <Animated.View style={{alignItems: 'center', opacity: fadeAnim}}>
+            <Animated.View
+              // eslint-disable-next-line react-native/no-inline-styles
+              style={{
+                zIndex: 2,
+                alignItems: "center",
+                opacity: fadeAnim,
+                position: 'absolute',
+                height: 100,
+                width: 400,
+                borderRadius: 10,
+                left: (width - 400) / 2,
+                top: height - 150}
+              }>
               <Button onPress={update} title="Update" />
             </Animated.View>
           </View>
@@ -120,18 +184,20 @@ const App = () => {
   );
 };
 
+
+
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: Colors.lighter,
+    backgroundColor: Colors.lighter
   },
   body: {
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.white
   },
   container: {
-    position: 'relative',
-    height: 500,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    position: "relative",
+    height: height,
+    justifyContent: "flex-end",
+    alignItems: "center"
   },
   map: {
     left: 0,
